d00e2dfd01cfce890d686d1e7acbcb6931f5dee3
oodinc <masoodinc1@gmail.com>
2025-03-16 07:46:57 +0700

    Remove description column from Alumni table in migration

---
+++ prisma/migrations/20250316004646_remove_alumni_description/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Alumni` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Alumni" DROP COLUMN "description";
